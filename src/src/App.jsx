6476950cf4bbfda929bfd271b05106a3cc5590e6
the3rdkaa-ops <the3rdkaa@gmail.com>
2026-01-31 01:01:08 +0900

    Update App.jsx

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -1,10 +1,1 @@
-import React from "react";
-import ArcticZoomedMap from "./ArcticZoomedMap";
-
-export default function App() {
-  return (
-    <div>
-      <ArcticZoomedMap />
-    </div>
-  );
-}
+src/App.jsx
