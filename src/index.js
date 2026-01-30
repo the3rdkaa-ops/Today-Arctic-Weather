a98748611f3841adf4f1d5fbaa98bd5d84faede9
the3rdkaa-ops <the3rdkaa@gmail.com>
2026-01-30 21:52:50 +0900

    Initialize React application with ArcticZoomedMap

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import ArcticZoomedMap from './ArcticZoomedMap';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ArcticZoomedMap />
+  </React.StrictMode>
+);
